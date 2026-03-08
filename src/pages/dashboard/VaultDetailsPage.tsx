11e7cc21733cfb1e051c2729045155907faad8e9
HemanthP06 <peddadahemanth6@gmail.com>
2026-03-08 06:40:03 +0000

    Add vault details page
    Co-authored-by: devndesigner6 <221837554+devndesigner6@users.noreply.github.com>

--- src/pages/dashboard/VaultDetailsPage.tsx
+++ src/pages/dashboard/VaultDetailsPage.tsx
@@ -63,7 +63,7 @@ export default function VaultDetailsPage() {
             // bytes -> try to decode as address (32 bytes)
             const bytes = Uint8Array.from(atob(item.value.bytes), c => c.charCodeAt(0))
             if (bytes.length === 32) {
-              globalState[key] = String(algosdk.encodeAddress(bytes))
+              globalState[key] = (algosdk.encodeAddress(bytes) as any as string)
             } else {
               globalState[key] = item.value.bytes
             }
