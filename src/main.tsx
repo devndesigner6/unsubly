a274cf8de056891915b4cc3a4a6526f119a3cd5c
HemanthP06 <peddadahemanth6@gmail.com>
2026-03-08 05:11:49 +0000

    Fix WalletConnect error
    Co-authored-by: devndesigner6 <221837554+devndesigner6@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Polyfill global for @perawallet/connect compatibility
+if (typeof global === 'undefined') {
+  // @ts-ignore
+  window.global = window;
+}
+
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
