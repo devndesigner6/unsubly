356539cf7709ca9406cd8918e8c5cf8d2defcab5
HemanthP06 <peddadahemanth6@gmail.com>
2026-03-12 18:10:23 +0000

    Fix modal to render UI
    Co-authored-by: devndesigner6 <221837554+devndesigner6@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
 import { Buffer } from "buffer/"
-;(window as any).Buffer = Buffer
-
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
-import { Toaster } from "./components/ui/toaster"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>
-)
+const globalScope = globalThis as any
+
+if (typeof globalScope.global === "undefined") {
+  globalScope.global = globalScope
+}
+
+if (typeof globalScope.process === "undefined") {
+  globalScope.process = { env: {}, version: "", browser: true }
+}
+
+if (typeof globalScope.Buffer === "undefined") {
+  globalScope.Buffer = Buffer
+}
+
+async function bootstrap() {
+  const { default: App } = await import("./App")
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+bootstrap()
